91a980b29d7bb5d1cf90e618448c327dae2b4c31
codespace-justin <justin@codespace.co.za>
2024-03-20 11:55:45 +0200

    adds conditional examples to apps.js

--- management-system/app.js
+++ management-system/app.js
@@ -1,8 +1,47 @@
 const CREATE_BTN = document.querySelector("[ data-system-controls='create']");
 
+// what truthy and falsy are, are essentially "thought" patterns of the computer
+// they are run on expression'
+
+// condition in if either resolves to "true" OR "false"
+
+// Arithmetic operators:
+// + - * / %
+
+// Logical Operators:
+// && - || - ! - ==
+
+let string; // variable declaration
+console.log(string) // undefined;
+
+string = "hello"; // variable assignment
+console.log(string) // hello
+ 
+let bool = true;
+
+
+if ( CREATE_BTN ) {
+    console.log("true");
+} else {
+    console.log("false");
+}
+
+let buttonExists = CREATE_BTN ? "true" : "false";
+
+const Fname = "justin"
+
+if (Fname == "Lebogang") {
+    console.log('you are wrong')
+} else if (Fname == "inolofatseng") {
+    console.log("you are even more wrong")
+} else if (Fname == "justin") {
+    console.log("you are the best")
+} else {
+    console.log("I dont know whats wrong with you, get help.")
+}
+
 const createBtnClickHandler = (event) => {
     alert("event fired")
-    console.log(event)
 }
 
 /**
@@ -15,4 +54,4 @@ CREATE_BTN.addEventListener("click", createBtnClickHandler)
 // parenthesis
 // function invocation
 // calling a function
-console.log()
+// console.log()
