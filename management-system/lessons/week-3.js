77a51534fab3c58a79d45319c8ed1ad7fd949af4
codespace-justin <justin@codespace.co.za>
2024-03-27 10:40:17 +0200

    updates lesson with theory for functions

--- management-system/lessons/week-3.js
+++ management-system/lessons/week-3.js
@@ -2,10 +2,10 @@
 const list = [];
 
 // functions - parenthesis
-/*
-    function exampleFunction() {}; // this is a function -> function expression
-    const exampleFunction = () => {} // a variable which stores function -> arrow function
-*/
+
+function exampleFunction() {}; // this is a function -> function expression
+// const exampleFunction = () => {} // a variable which stores function -> arrow function
+
 
 // object - braces
 const thing = {};
@@ -29,7 +29,30 @@ console.log(TASKS) // length 5
 TASKS[1] = "jsl-10";
 console.log(TASKS)  // length 5
 
+// deleting an element from the end of an array
 TASKS.pop();  
-console.log(TASKS)// length 4
+console.log(TASKS) // length 4
+
+// delete 1 element from index 2
+// 1st arg: index of element to be deleted
+// 2nd arg: number of items to be deleted
+TASKS.splice(2, 1);
+console.log(TASKS); // length 2
 
 // ================================== Functions and Methods ======================================
+
+// function is a reuseable code block that is used to group related functionality together
+/**
+ * create abstractions
+ * they have their own block scope - block statements
+ * they can accept arguments
+ * they make return statements which are able to return values back to the global scope
+ * arrow vs function declarations
+ * functions vs methods
+ * 
+ * Syntax for a funciton: function name | parenthesis | braces {} for body of function
+ */
+
+function createTask() {
+    
+}
