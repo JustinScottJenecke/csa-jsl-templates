8933f0376d1bf1f17b98d2065d9a4c286353c173
JustinScottJenecke <justinjenecke@gmail.com>
2024-04-17 10:22:09 +0200

    creates multi line ternary

--- todo-app/app.js
+++ todo-app/app.js
@@ -11,15 +11,25 @@ const createTodoForm = {
 // const createTodoFormContainer = document.querySelector('#create-todo-form-container')
 // let createTodoFormState = true;
 
-console.log(createTodoForm.showing)
+// console.log(createTodoForm.showing)
 
 // console.log(createTodoToggleBtn, createTodoFormContainer)
 createTodoForm.toggleBtn.addEventListener('click', () => {
     
     createTodoForm.showing = !createTodoForm.showing
-     console.log(createTodoForm.showing)
+        console.log(createTodoForm.showing)
 
-     createTodoForm.showing ? createTodoForm.container.classList.remove('hidden') : createTodoForm.container.classList.add('hidden')
+        createTodoForm.showing 
+            ? 
+            ( 
+                createTodoForm.container.classList.remove('hidden'),
+                createTodoForm.toggleBtn.innerText = "Close Form"
+            )   
+            :
+            (
+                createTodoForm.container.classList.add('hidden'),
+                createTodoForm.toggleBtn.innerText = "Create Todo"
+            )
 })
 
 
@@ -58,3 +68,11 @@ fetch('./data.json')
     .then(response => response.json())
     .then(data => console.log(data))
     .catch(error => console.error('Error:', error));
+
+
+/**
+ * swap text on show form button
+ * creating todo items - grab data from forms, create todo objects, append them to DOM
+ * load in dummy todo items from local storage - setup local storage, load in data from local storage
+ * setup local storage with data from json file
+ */
